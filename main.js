4d2a15216f1a401bd5496feacf706b1268d817b3
erikagiella <erikagiella93@gmail.com>
2025-07-11 18:24:28 +0200

    Ricalcolo costo del biglietto per le fasce d'età in sconto + stampa del risultato "in forma umana"

--- main.js
+++ main.js
@@ -14,36 +14,45 @@
 // 100km, 70 anni => prezzo corretto: €12.60
 
 /* Tools:
-- const/let (per km, età, prezzo a km, risultato)
+- const/let (per prezzo a km, costo biglietto)
 - prompt (per km ed età)
 - if/else if/else  (per calcolo biglietto)
-- console.log (per stampa risultato)
-- metodo ToFixed () (per arrotondamento risultato a 2 decimali)
+- console.log (per stampa costo biglietto)
+- metodo ToFixed () (per arrotondamento costo biglietto a 2 decimali)
 */
 
 
 // Procedimento:
 
-//creo le variabili in cui metterò prezzo a km e risultato
-const prezzoAlChilometro = 0.21;
-let risultato;
+//creo le variabili in cui metterò prezzo a km e costo biglietto
+const prezzoAlChilometro = 0.21
+let costoBiglietto
 
 //chiedo all'utente la quantità di km e la assegno alla relativa variabile
-const chilometriDaPercorrere = prompt('Quanti km vuoi percorrere?');
+const chilometriDaPercorrere = prompt('Quanti km vuoi percorrere?')
 //chiedo all'utente l'età del passeggero e la assegno alla relativa variabile
-const etàPasseggero = prompt('Qual è l età del passeggero?');
+const etàPasseggero = prompt('Qual è l età del passeggero?')
 //calcolo il prezzo del biglietto
-risultato = chilometriDaPercorrere * prezzoAlChilometro;
+costoBiglietto = chilometriDaPercorrere * prezzoAlChilometro
 
 
 //IF/ELSE IF/ELSE
 
 // SE il passeggero ha meno di 18 anni, al risultato verrà scontato il 20%
+if (etàPasseggero < 18) {
+    const sconto = costoBiglietto * 20 / 100
+    costoBiglietto = costoBiglietto - sconto
+}
 
 // ALTRIMENTI SE il passeggero ha più di 65 anni, al risultato verrà scontato il 40%
+else if (etàPasseggero > 65) {
+    const sconto = costoBiglietto * 40 / 100
+    costoBiglietto = costoBiglietto - sconto
+} else {
+    // else superfluo in quanto negli altri casi il costo 
+}
 
 //stampo il risultato con due cifre decimali
-
-console.log(risultato)
+console.log(`Il biglietto per percorrere ${chilometriDaPercorrere} km per un passeggero di ${etàPasseggero} anni è di ${costoBiglietto.toFixed(2)} euro`)
 
 
